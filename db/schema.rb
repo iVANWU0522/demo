9130acc0d4978ecf79226c31f92d9c2f8fcf633a
IvanWu <ivanwu@debian>
2017-09-26 21:25:37 +1000

    Initial commit

---
+++ db/schema.rb
@@ -0,0 +1,71 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170823133156) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "event_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "position",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "event_groupships", force: :cascade do |t|
+    t.integer  "event_id",   limit: 4
+    t.integer  "group_id",   limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.boolean  "is_public"
+    t.integer  "capacity",    limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "status",      limit: 255
+    t.integer  "category_id", limit: 4
+  end
+
+  add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "event_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "bio",        limit: 65535
+    t.date     "birthday"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+end
